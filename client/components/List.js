3781096c795336265b9952aa90561a5b5daabed6
Tommy Ngo <tommyhoaingo@gmail.com>
2022-01-19 15:21:54 -0600

    clean slate

--- client/components/List.js
+++ client/components/List.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Card from '../components/Card.js';
 import useStore from '../store.js';
 import list from '../components/styles/list.css';
-// import AddMedia from "./AddMedia"
 
 function List() {
   const list = useStore((state) => state.list);
@@ -24,16 +22,6 @@ function List() {
         console.log('error: ', err);
       })
   }, []);
-
-  // const components = allMedia.map(obj => {
-  //   // line 8: every card requires a unique key, which can be assigned to the media_id since those keys are always unique
-  //   return (
-  //     <div key={obj.media_id}>
-  //       <Card media_id={obj.media_id} title={obj.title} category={obj.category} duration={obj.duration} priority={obj.priority} url={obj.url} user_id={obj.user_id}/>
-  //     </div>
-  //   );
-  // });
-
   return (
    
    <div className='container'>
