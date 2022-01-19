506b4106b13a47cf776483b8c3790261a07b5970
Tommy Ngo <tommyhoaingo@gmail.com>
2022-01-19 15:26:44 -0600

    added buttons

--- client/components/List.js
+++ client/components/List.js
@@ -14,33 +14,34 @@ function List() {
         'Content-Type': 'application/json',
       },
     })
-      .then(res => res.json())
-      .then(data => {
-        setList(data)
+      .then((res) => res.json())
+      .then((data) => {
+        setList(data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log('error: ', err);
-      })
+      });
   }, []);
   return (
-   
-   <div className='container'>
-      <div>{list.map(obj =>
-        <div key={obj.imdbid} className='box is-flex is-flex-direction-row is-justify-content-space-around is-align-items-center'>
-          <div>
-            <img src={obj.poster}></img>
+    <div className='container'>
+      <div>
+        {list.map((obj) => (
+          <div
+            key={obj.imdbid}
+            className='box is-flex is-flex-direction-row is-justify-content-space-around is-align-items-center'
+          >
+            <div>
+              <img src={obj.poster}></img>
+            </div>
+            <ul>{obj.title}</ul>
+            <ul>{obj.year}</ul>
+            <button>up</button>
+            <button>down</button>
+            <button>delete</button>
           </div>
-
-          {/* <div className='textContainer'> */}
-          <ul>{obj.title}</ul>
-          <ul>{obj.year}</ul>
-          {/* </div> */}
-        </div>
-      )}
+        ))}
       </div>
     </div>
-
-    
   );
 }
 
